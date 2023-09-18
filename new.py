4621d46696dcb0c1ad2c4e60b5b35abeb0c0d94b
Wole Fabikun <wole359@gmail.com>
2023-09-17 23:21:15 -0400

    Added code for the NSRDB application

--- new.py
+++ new.py
@@ -1,1 +1,85 @@
-# new NSRDB code goes here
+import requests
+import pandas as pd
+import urllib.parse
+import time
+
+API_KEY = "GYBF1zDpAFqOwBM1j5B3dDf951XUn1dGg2P7oren"
+EMAIL = "wole359@gmail.com"
+BASE_URL = "https://developer.nrel.gov/api/nsrdb/v2/solar/psm3-2-2-download.json?"
+POINTS = [
+'1338345'
+]
+
+def main():
+    input_data = {
+        'attributes': 'air_temperature,clearsky_dhi,clearsky_dni,solar_zenith_angle,surface_albedo,ghi',
+        'interval': '60',
+        'to_utc': 'false',
+        'include_leap_day': 'true',
+        
+        'api_key': API_KEY,
+        'email': EMAIL,
+    }
+    for name in ['2021','2020','2019','2018']:
+        print(f"Processing name: {name}")
+        for id, location_ids in enumerate(POINTS):
+            input_data['names'] = [name]
+            input_data['location_ids'] = location_ids
+            print(f'Making request for point group {id + 1} of {len(POINTS)}...')
+
+            if '.csv' in BASE_URL:
+                url = BASE_URL + urllib.parse.urlencode(data, True)
+                # Note: CSV format is only supported for single point requests
+                # Suggest that you might append to a larger data frame
+                data = pd.read_csv(url)
+                print(f'Response data (you should replace this print statement with your processing): {data}')
+                # You can use the following code to write it to a file
+                # data.to_csv('SingleBigDataPoint.csv')
+            else:
+                headers = {
+                  'x-api-key': API_KEY
+                }
+                data = get_response_json_and_handle_errors(requests.post(BASE_URL, input_data, headers=headers))
+                download_url = data['outputs']['downloadUrl']
+                # You can do with what you will the download url
+                print(data['outputs']['message'])
+                print(f"Data can be downloaded from this url when ready: {download_url}")
+
+                # Delay for 1 second to prevent rate limiting
+                time.sleep(1)
+            print(f'Processed')
+
+
+def get_response_json_and_handle_errors(response: requests.Response) -> dict:
+    """Takes the given response and handles any errors, along with providing
+    the resulting json
+
+    Parameters
+    ----------
+    response : requests.Response
+        The response object
+
+    Returns
+    -------
+    dict
+        The resulting json
+    """
+    if response.status_code != 200:
+        print(f"An error has occurred with the server or the request. The request response code/status: {response.status_code} {response.reason}")
+        print(f"The response body: {response.text}")
+        exit(1)
+
+    try:
+        response_json = response.json()
+    except:
+        print(f"The response couldn't be parsed as JSON, likely an issue with the server, here is the text: {response.text}")
+        exit(1)
+
+    if len(response_json['errors']) > 0:
+        errors = '\n'.join(response_json['errors'])
+        print(f"The request errored out, here are the errors: {errors}")
+        exit(1)
+    return response_json
+
+if __name__ == "__main__":
+    main()
